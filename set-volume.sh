2bbf58e3a8bacb51a3216475cd7b941844297bd5
Fernando Bombardelli <bombardelli.f@gmail.com>
2021-03-08 12:02:40 -0300

    Updating

--- set-volume.sh
+++ set-volume.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
-ctrl=`amixer scontents | head -1 | egrep -oE "control '\w+'" | cut -d ' ' -f2`
+# ctrl=`amixer scontents | head -1 | egrep -oE "control '\w+'" | cut -d ' ' -f2`
+# amixer set $ctrl -q $1
 
-amixer set $ctrl -q $1
+pactl -- set-sink-volume @DEFAULT_SINK@ $1
